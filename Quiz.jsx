5da1090465287a20e6cee5137177ddabc178bf11
Sicoros Amand <sicoros955@jomspar.com>
2025-03-20 13:50:35 +0530

    Addede header, quiz

---
+++ Quiz.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function Quiz() {
+
+    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    return
+        <p>
+            Currently active Question
+        </p>;
+    
+}
+
+export default Quiz
